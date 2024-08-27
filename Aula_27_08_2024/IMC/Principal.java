1a67285f9f1d67453bce301758280795737e33ee
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-08-27 11:16:26 -0300

    Create Principal.java

---
+++ Aula_27_08_2024/IMC/Principal.java
@@ -0,0 +1,20 @@
+package imc_27_08_2024;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//scanner
+		
+		//objeto
+		
+		//passar os dados de leitura
+		
+		//calcular o IMC
+		
+		//resultado
+
+	}
+
+}
