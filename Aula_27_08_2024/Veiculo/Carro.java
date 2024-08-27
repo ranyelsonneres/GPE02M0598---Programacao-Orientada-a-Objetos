e72773fd068b17159cce4010b79ce856940e6b39
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-08-27 11:14:41 -0300

    Create Carro.java

---
+++ Aula_27_08_2024/Veiculo/Carro.java
@@ -0,0 +1,21 @@
+package veiculo_27_08_2024;
+
+public class Carro {
+	
+	//Classe
+	//Atributos (características)
+	String modelo;
+	String cor;
+	int ano;
+	String placa;
+	
+	public void exibirInformacoes() {
+		System.out.println("Modelo: " + modelo);
+		System.out.println("Cor: " + cor);
+		System.out.println("Ano: " + ano);
+		System.out.println("Placa: " + placa);
+	}
+	
+	
+
+}
