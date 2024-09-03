244bd814ffd4d59be5a216a6434445d9aea95c0f
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-09-03 11:12:56 -0300

    Update Principal.java

--- Aula_27_08_2024/IMC/Principal.java
+++ Aula_27_08_2024/IMC/Principal.java
@@ -1,19 +1,36 @@
 package imc_27_08_2024;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
 		//scanner
+		Scanner sc = new Scanner(System.in);
 		
 		//objeto
+		CalculadoraIMC calc = new CalculadoraIMC();
 		
 		//passar os dados de leitura
+		System.out.println("Peso: ");
+		calc.peso = sc.nextDouble();
+		
+		System.out.println("Altura: ");
+		calc.altura = sc.nextDouble();
 		
 		//calcular o IMC
+		double imc = calc.calcularIMC();
+		
 		
 		//resultado
+		String resultadoIMC = calc.interpretarIMC(imc);
+		
+		System.out.println("IMC: " + imc);
+		System.out.println("Classificacao: " + resultadoIMC);
+		
+		sc.close();
 
 	}
 
