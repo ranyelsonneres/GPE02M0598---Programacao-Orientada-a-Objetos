217cc83e415bcc8b0095c43970ce0c2a2c4d7cd5
Ranyelson Neres <ranyelson.carvalho@p.ucb.br>
2024-11-12 13:58:01 -0300

    Create Main.java

---
+++ Aula_12_11_2024/Main.java
@@ -0,0 +1,11 @@
+package interface_grafica;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MinhaJanela minhaJanela = new MinhaJanela();
+		minhaJanela.setVisible(true);
+	}
+
+}
